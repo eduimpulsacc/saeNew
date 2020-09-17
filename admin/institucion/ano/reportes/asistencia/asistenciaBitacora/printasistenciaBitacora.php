22606079f23f6cb217faa00044833455094edc19
eduimpulsacc <claudia.canto@eduimpulsa.com>
2020-09-17 15:41:35 -0300

    reporte asistencia bitacora

--- admin/institucion/ano/reportes/asistencia/asistenciaBitacora/printasistenciaBitacora.php
+++ admin/institucion/ano/reportes/asistencia/asistenciaBitacora/printasistenciaBitacora.php
@@ -12,17 +12,25 @@ include('../../../../../clases/class_Reporte.php');
 include('../../../../../clases/class_Membrete.php');
 setlocale(LC_ALL,"es_ES");
 	//setlocale("LC_ALL","es_ES");
+	
+	
+foreach($_POST as $nombre_campo => $valor){
+$asignacion = "\$" . $nombre_campo . "='" . $valor . "';";
+eval($asignacion);
+}
+
 	$institucion	=$_INSTIT;
 	$ano			=$_ANO;
 	$mes 			= $cmb_mes;
 	$ense			= $cmb_ense;
 	$reporte		=$c_reporte;
-	$curso=1;	
+	$curso			=$cmb_curso;	
 	$_POSP = 4;
 	$_bot = 8;
 
 
-   
+
+
   /* echo "parte: $numero_ini<br>";
     echo "termina: $numero_fin<br>";*/
    
@@ -74,9 +82,48 @@ setlocale(LC_ALL,"es_ES");
 	$numero_fin = cal_days_in_month(CAL_GREGORIAN, $mes, $nro_ano); // 31
 	
 	$ob_reporte->ano=$ano;
-	$ob_reporte->ensenanza=$ense;
-	$rs_cursos = $ob_reporte->ListadoCurso($conn);
+
+
+$fechaInicio = CambioFE($desde);
+$fechaFin = CambioFE($hasta);
+
+# Fecha como segundos
+$tiempoInicio = strtotime($fechaInicio);
+$tiempoFin = strtotime($fechaFin);
+# 24 horas * 60 minutos por hora * 60 segundos por minuto
+$dia = 86400;
+$co=0;
+while($tiempoInicio <= $tiempoFin){
+	# Podemos recuperar la fecha actual y formatearla
+	# Más información: http://php.net/manual/es/function.date.php
+	$fechaActual = date("Y-m-d", $tiempoInicio);
+	//printf("Fecha dentro del ciclo: %s\n", $fechaActual);
 	
+	if(!EsSabadoDomingo($fechaActual)){
+		
+		$ob_reporte->curso = $curso;
+		$ob_reporte->fecha = $fechaActual;
+		$rs_asig = $ob_reporte->getAsigBitacoraFecha($conn);
+		
+		if(pg_numrows($rs_asig)>0){
+			$arr_fechas[$co]['fecha']=$fechaActual;
+			$arr_fechas[$co]['cols']=pg_numrows($rs_asig);
+			$co++;
+		}
+	}
+
+	# Sumar el incremento para que en algún momento termine el ciclo
+	$tiempoInicio += $dia;
+	
+}
+
+
+
+$ob_reporte ->curso = $curso;
+$ob_reporte ->ano = $ano;
+$ob_reporte ->retirado =0;
+//$ob_reporte ->orden =$ck_orden;
+$result_alu =$ob_reporte ->TraeTodosAlumnos($conn);
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -231,7 +278,111 @@ if (empty($curso) or empty($desde) or empty($hasta)){
     <td>&nbsp;</td>
     </tr>
   
-</table>	
+</table><br>
+<br>
+<br>	
+<table width="826" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" class="tableindex"><div align="center"> INFORME DE ASISTENCIA SEG&Uacute;N BIT&Aacute;CORA</div></td>
+  </tr>
+  <tr>
+    <td align="center" class="textosimple">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" class="textosimple"><?php echo CursoPalabra($curso,1,$conn) ?></td>
+  </tr>
+  <tr>
+    <td align="center"><span class="Estilo2"><strong><?php echo $desde ?> a <?php echo $hasta ?></strong></span></td>
+  </tr>
+  <tr>
+    <td align="center" class="Estilo2">&nbsp;</td>
+  </tr>
+</table>
+<br><br>
+<table  border="1" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse">
+  <tr class="tableindex">
+    <td rowspan="2" align="center">ALUMNO</td>
+   <?php  for($f=0;$f<count($arr_fechas);$f++){
+	   
+	   
+	   ?>
+    <td  nowrap colspan="<?php echo $arr_fechas[$f]['cols'] ?>"><div align="center">&nbsp;<?php echo CambioFD($arr_fechas[$f]['fecha']) ?>&nbsp;</div></td>
+  <?php  
+		
+   }?>
+    <td rowspan="2" align="center">Cantidad</td>
+    <td rowspan="2" align="center">Debi&oacute; asistir</td>
+    <td rowspan="2" align="center">% Asistencia</td>
+  </tr>
+  <tr class="tableindex">
+     <?php  for($f=0;$f<count($arr_fechas);$f++){
+		$ob_reporte->curso = $curso;
+		$ob_reporte->fecha = $arr_fechas[$f]['fecha'];
+		$rs_asig = $ob_reporte->getAsigBitacoraFecha($conn);
+		 for($r=0;$r<pg_numrows($rs_asig);$r++){
+		$fila_asig = pg_fetch_array($rs_asig,$r);
+		 ?>
+         
+    <td>
+    <div align="center">
+  <?php echo  Iniciales($fila_asig['nombre']); ?>
+    </div></td>
+     <?php }
+    }?>
+  </tr>
+   <?php  for($a=0;$a<pg_numrows($result_alu);$a++){
+		   $fila_alu = pg_fetch_array($result_alu,$a);
+		   $rut_alumno  = $fila_alu['rut_alumno'];
+		   $salu=0;
+		   
+		   ?>
+  <tr class="textosimple">
+    <td><? $nombre_alumno = ucwords(strtoupper(trim($fila_alu['ape_pat']) .  " " . trim($fila_alu['nombre_alu'])));
+	  echo $ob_reporte->tildeM($nombre_alumno);  ?></td>
+    <?php  for($f=0;$f<count($arr_fechas);$f++){
+		$ob_reporte->curso = $curso;
+		$ob_reporte->fecha = $arr_fechas[$f]['fecha'];
+		$rs_asig = $ob_reporte->getAsigBitacoraFecha($conn);
+		 for($r=0;$r<pg_numrows($rs_asig);$r++){
+		$fila_asig = pg_fetch_array($rs_asig,$r);
+		 ?>
+         
+    <td align="center">
+   <?php  
+    $ob_reporte->ramo =$fila_asig['id_ramo']; 
+    $cuentaBi = $ob_reporte->getCantBitacoraDR($conn);
+	$tbi[$rut_alumno][]=$cuentaBi;
+	
+	$ob_reporte->alumno =$rut_alumno;
+	$cuentaAsis = $ob_reporte->getCantAsisBitacora($conn);
+	 
+	?>
+    <div align="center">
+    <?php  if($cuentaAsis>0){
+		 echo $cuentaAsis;
+		$tai[$rut_alumno][]=$cuentaAsis;
+	 }
+	 else{
+		echo 0; 
+		}?>
+    </div></td>
+     <?php }
+    }?>
+    <td align="center">
+	<?php  $posibles = array_sum($tbi[$rut_alumno]);
+	echo intval($posibles);
+	 ?></td>
+    <td align="center">
+    <?php 
+	 $asis = array_sum($tai[$rut_alumno]);
+	 echo intval($asis);
+	?>
+    </td>
+    <td align="center"><?php echo round(($asis*100)/$posibles) ?></td>
+  </tr>
+  <?php }?>
+</table>
+	
 <?	
 }
 
